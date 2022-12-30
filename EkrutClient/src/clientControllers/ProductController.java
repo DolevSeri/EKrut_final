dd74da429a3b1d7c3bfb1f5585b33eee7dd366cc
user <user@MININT-O6P73GH>
2022-12-30 03:25:16 +0200

    order cart and catalog working..

--- EkrutClient/src/clientControllers/ProductController.java
+++ EkrutClient/src/clientControllers/ProductController.java
@@ -1,5 +1,11 @@
 package clientControllers;
 
+import java.io.IOException;
+
+import client.ClientUI;
+import entities.Product;
+import entities.ProductInDevice;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,9 +13,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ProductController {
+	private static Client_OrderScreenController client_OrderScreenController;
 
 	@FXML
 	private Button addToCart;
+
+	@FXML
+	private Label lblError;
 
 	@FXML
 	private Label lblName;
@@ -23,11 +33,48 @@ public class ProductController {
 	@FXML
 	private ImageView productLogo;
 
-	public void setData(String name, double price, String sale, String imagePath) {
-		lblName.setText(name);
-		lblPrice.setText(String.valueOf(price));
+	private ProductInDevice product;
+	private int quantityInOrder = 1;
+
+	/**
+	 * SetData-a method that set the fxml of product in the product catalog screen
+	 * 
+	 * @param name-will                    contain the name of the products
+	 * @param price-will                   contain the price of thr products
+	 * @param sale-will                    contain the sale that the region have
+	 * @param imagePath-                   will contain the path of the product
+	 *                                     image
+	 * @param client_OrderScreenController - contain the main order screen
+	 *                                     controller/
+	 */
+
+	public void setData(ProductInDevice product, String sale,
+			Client_OrderScreenController client_OrderScreenController) {
+		this.product = product;
+		lblName.setText(product.getProductName());
+		lblPrice.setText(String.valueOf(product.getPrice()));
 		lblSale.setText(sale);
-		Image image = new Image(imagePath);
+		Image image = new Image(product.getImagePath());
 		productLogo.setImage(image);
+		this.client_OrderScreenController = client_OrderScreenController;
+	}
+
+	@FXML
+	void clickOnAdd(ActionEvent event) throws IOException {
+		if (product.getQuantity() == 0) {
+			lblError.setText("Out of stock!");
+		} else {
+			product.setQuantity(product.getQuantity() - 1); // update the quantity in device.
+			client_OrderScreenController.selectedProducts.put(product, quantityInOrder++);
+			client_OrderScreenController.setCartGrid(product,this);
+		}
+	}
+
+	public int getQuantityInOrder() {
+		return quantityInOrder;
+	}
+
+	public ProductInDevice getProductInDevice() {
+		return product;
 	}
 }
---
+++ EkrutClient/src/clientControllers/ProductInCartController.java
@@ -0,0 +1,69 @@
+package clientControllers;
+
+import entities.ProductInDevice;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ProductInCartController {
+	private Client_OrderScreenController client_OrderScreenController;
+	private ProductInDevice product;
+	private ProductController productController;
+	private int indexInCart;
+	@FXML
+	private Button btnMinus;
+
+	@FXML
+	private ImageView imgProduct;
+
+	@FXML
+	private Label lblName;
+
+	@FXML
+	private Label lblPrice;
+
+	@FXML
+	private Label lblQuantity;
+
+	public void setData(ProductInDevice product, Client_OrderScreenController client_OrderScreenController) {
+		this.product = product;
+		lblName.setText(product.getProductName());
+		for (ProductController p : client_OrderScreenController.productControllers) {
+			if (p.getProductInDevice().getProductName().equals(product.getProductName())) {
+				lblPrice.setText(String.valueOf((p.getQuantityInOrder() - 1) * product.getPrice()));
+				productController = p;
+				break;
+			}
+		}
+		lblQuantity.setText(String.valueOf(client_OrderScreenController.selectedProducts.get(product)));
+		Image image = new Image(product.getImagePath());
+		imgProduct.setImage(image);
+		this.client_OrderScreenController = client_OrderScreenController;
+	}
+
+	public ProductController getProductController() {
+		return productController;
+	}
+
+	public void setProductController(ProductController productController) {
+		this.productController = productController;
+
+	}
+
+	public void setIndexInCart(int indexInCart) {
+		this.indexInCart = indexInCart;
+	}
+
+	public int getIndexInCart() {
+		return indexInCart;
+	}
+
+	@FXML
+	void clickOnMinus(ActionEvent event) {
+
+	}
+
+}
