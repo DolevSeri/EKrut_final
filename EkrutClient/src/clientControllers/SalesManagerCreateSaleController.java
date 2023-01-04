9d45c2a9fb5793b508f6daa63f2dce00167dd83c
peleg2719 <peleg2719@gmail.com>
2023-01-04 21:32:30 +0200

    SALE

---
+++ EkrutClient/src/clientControllers/SalesManagerCreateSaleController.java
@@ -0,0 +1,57 @@
+package clientControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+
+public class SalesManagerCreateSaleController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnSendSale;
+
+    @FXML
+    private Button btnexit1;
+
+    @FXML
+    private ComboBox<?> cmbArea;
+
+    @FXML
+    private ComboBox<?> cmbSale;
+
+    @FXML
+    private ImageView icon;
+
+    @FXML
+    private ImageView saleImage;
+
+    @FXML
+    void ClickOnSendToAcrivate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseAreaComboBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseSalePatternComboBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickOnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getExitBtn(ActionEvent event) {
+
+    }
+
+}
---
+++ EkrutClient/src/clientControllers/SalesWorkerActivateSaleController.java
@@ -0,0 +1,37 @@
+package clientControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class SalesWorkerActivateSaleController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnexit1;
+
+    @FXML
+    private GridPane gpSalesList;
+
+    @FXML
+    private AnchorPane gpUsers;
+
+    @FXML
+    private ImageView saleImage;
+
+    @FXML
+    void clickOnBtnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getExitBtn(ActionEvent event) {
+
+    }
+
+}
