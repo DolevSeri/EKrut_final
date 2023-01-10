71d2ba109ab230e96991aba49c92d832253b6ca4
Dolev <dolevseri12@gmail.com>
2023-01-10 19:21:27 +0200

    To Inbar

--- EkrutClient/src/clientControllers/AreaManager_InventoryCallsController.java
+++ EkrutClient/src/clientControllers/AreaManager_InventoryCallsController.java
@@ -14,62 +14,46 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
-/**
- * This class is the controller for the Area Manager Inventory Calls form. It
- * handles the actions of the buttons and initializes the table view.
- *
- * @author Eden Bar
- */
 public class AreaManager_InventoryCallsController {
 
-	@FXML
-	private Button btnBack;
+    @FXML
+    private Button btnexit;
 
-	@FXML
-	private Button btnCreateCall;
+    @FXML
+    private Button btnBack;
 
-	@FXML
-	private Button btnexit;
+    @FXML
+    private Button btnCreateCall;
 
-	@FXML
-	private Label lblError;
+    @FXML
+    private Label lblError;
 
-	@FXML
-	private TableColumn<InventoryCall, Integer> clCallID;
+    @FXML
+    private ComboBox<String> cmbDevice;
 
-	@FXML
-	private TableColumn<InventoryCall, CallStatus> clStatus;
+    @FXML
+    private ComboBox<String> cmbProduct;
 
-	@FXML
-	private TableColumn<InventoryCall, String> clDeviceName;
 
-	@FXML
-	private TableColumn<InventoryCall, String> clProduct;
-
-	@FXML
-	private TableView<InventoryCall> tblCalls;
-
-	@FXML
-	private Button btnCloseCall;
-
-	private TableViewController myTable = new TableViewController();
 	private String area = ChatClient.userController.getUser().getRegion().toString();
 	private SetSceneController scene = new SetSceneController();
 
 	/**
-	 * Initializes the table view and sets the columns and items.
+	 * The initialize method is called by JavaFX when the view is being set up. It
+	 * hides certain labels, disables the product combo box, sets the prompt text
+	 * for the product combo box, and gets the list of devices for the user's area
+	 * to populate the device combo box.
 	 */
-	@FXML
 	public void initialize() {
-		tblCalls.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		setColumns();
-		setTableItems();
+		cmbProduct.setDisable(true);
+		cmbProduct.setPromptText("Choose Device First");
+		ClientUI.chat.accept(new Message(Request.Get_Devices_By_Area, area));
+		cmbDevice.getItems().addAll(ChatClient.deviceController.getAreaDevicesNames());
 	}
 
 	/**
@@ -82,6 +66,21 @@ public class AreaManager_InventoryCallsController {
 		scene.back(event, "/clientGUI/AreaManager_InventoryManagementForm.fxml");
 	}
 
+	@FXML
+	void clickComboDevice(ActionEvent event) {
+		cmbProduct.getItems().clear();
+		cmbProduct.setDisable(false);
+		String deviceChosen = null;
+		try {
+			deviceChosen = cmbDevice.getValue().toString();
+		} catch (NullPointerException e) {
+		}
+		
+		ClientUI.chat.accept(new Message(Request.Get_Products, deviceChosen));
+		cmbProduct.getItems().addAll(ChatClient.productCatalogController.getProductsInDevicesNames());
+		cmbProduct.setPromptText("Choose Product");
+	}
+	
 	/**
 	 * Handles the action of clicking the create new call button.
 	 *
@@ -89,8 +88,12 @@ public class AreaManager_InventoryCallsController {
 	 */
 	@FXML
 	void clickCreateNewCallBtn(ActionEvent event) {
-		((Node) event.getSource()).getScene().getWindow().hide(); // hiding primary window
-		scene.setScreen(new Stage(), "/clientGUI/AreaManager_NewCallForm.fxml");
+		ArrayList<String> fields = new ArrayList<String>(Arrays.asList(cmbDevice.getValue(), cmbProduct.getValue()));
+		if (fields.contains(null)) {
+			scene.popUpMessage("You need to choose Device and Product!");
+		} else {
+			ClientUI.chat.accept(new Message(Request.Create_Inventory_Call, fields));
+		}
 
 	}
 
@@ -109,31 +112,31 @@ public class AreaManager_InventoryCallsController {
 	 * @param event         the action event that triggered this method call
 	 */
 
-	@FXML
-	void clickCloseCallBtn(ActionEvent event) {
-		ObservableList<InventoryCall> selectedCalls = tblCalls.getSelectionModel().getSelectedItems();
-		if (selectedCalls.isEmpty()) {
-			lblError.setText("Error: No calls selected");
-			lblError.setVisible(true);
-		} else {
-			boolean allDone = true;
-			for (InventoryCall call : selectedCalls) {
-				if (call.getStatus() != CallStatus.DONE) {
-					allDone = false;
-					break;
-				}
-			}
-			if (!allDone) {
-				lblError.setText("Error: Cannot close calls with a status other than" + CallStatus.DONE.toString());
-				lblError.setVisible(true);
-			} else {
-				lblError.setVisible(false);
-				ArrayList<InventoryCall> callsToClose = new ArrayList<>(selectedCalls);
-				ClientUI.chat.accept(new Message(Request.Inventory_Calls_To_Close, callsToClose));
-				setTableItems();
-			}
-		}
-	}
+//	@FXML
+//	void clickCloseCallBtn(ActionEvent event) {
+//		ObservableList<InventoryCall> selectedCalls = tblCalls.getSelectionModel().getSelectedItems();
+//		if (selectedCalls.isEmpty()) {
+//			lblError.setText("Error: No calls selected");
+//			lblError.setVisible(true);
+//		} else {
+//			boolean allDone = true;
+//			for (InventoryCall call : selectedCalls) {
+//				if (call.getStatus() != CallStatus.DONE) {
+//					allDone = false;
+//					break;
+//				}
+//			}
+//			if (!allDone) {
+//				lblError.setText("Error: Cannot close calls with a status other than" + CallStatus.DONE.toString());
+//				lblError.setVisible(true);
+//			} else {
+//				lblError.setVisible(false);
+//				ArrayList<InventoryCall> callsToClose = new ArrayList<>(selectedCalls);
+//				ClientUI.chat.accept(new Message(Request.Inventory_Calls_To_Close, callsToClose));
+//				setTableItems();
+//			}
+//		}
+//	}
 
 	/**
 	 * Handles the action of clicking the exit button.
@@ -144,27 +147,4 @@ public class AreaManager_InventoryCallsController {
 	void clickExitBtn(ActionEvent event) {
 		scene.exitOrLogOut(event, false);
 	}
-
-	/**
-	 * Sets the columns in the table to display the specified fields.
-	 */
-	private void setColumns() {
-		myTable.setColumn(clCallID, "callID");
-		myTable.setColumn(clStatus, "Status");
-		myTable.setColumn(clDeviceName, "deviceName");
-		myTable.setColumn(clProduct, "productName");
-
-	}
-	
-	/**
-	 * Clears the items in the table and sets them to the list of inventory calls for the current area.
-	 */
-	private void setTableItems() {
-		tblCalls.getItems().clear();
-		ArrayList<String> areaAndStatus = new ArrayList<>();
-		areaAndStatus.addAll(Arrays.asList(area, null));
-		ClientUI.chat.accept(new Message(Request.Get_Inventory_Calls_By_Area, areaAndStatus));
-		tblCalls.setItems(ChatClient.inventoryCallController.getAreaCalls());
-	}
-
 }
