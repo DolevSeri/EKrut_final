171514274dfd35eadcd38738c04bf24c67e6bd37
peleg2719 <peleg2719@gmail.com>
2023-01-18 12:39:34 +0200

    PELEGTEST

--- EkrutClient/src/client/ScreenInterface.java
+++ EkrutClient/src/client/ScreenInterface.java
@@ -6,4 +6,6 @@ public interface ScreenInterface {
     public String getTxtUsername();
     public String getTxtPswd();
     public void changeScreen(Stage stage,String path);
+	public void SetTextLableErrorUserNotExist();
+	public void SetTextLableErrorUserAlreadyLogIn();
 }
